f51ded3f5d95caa2179c594e614ca7c419152b05
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2026-02-01 19:04:25 +0100

    gg

---
+++ Movie.java
@@ -0,0 +1,10 @@
+public class Movie{
+public static void main(String[] args) {
+    System.out.println("Man On Fire");
+    System.out.println("Denzel washington");
+    System.out.println("2004");
+}
+
+
+
+}
