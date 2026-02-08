516022b50c6b774514fc62680402b5c33a57260b
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2026-02-08 13:48:12 +0100

    123

---
+++ 2/ArithmeticComparison.java
@@ -0,0 +1,21 @@
+package 2;
+
+public class ArithmeticComparison {
+ 
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic operations
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a * b = " + (a * b));
+
+        // Comparison operations
+         //true
+        System.out.println("a > b = " + (a > b));
+         //false
+        System.out.println("a == b = " + (a == b));
+    }
+}
+
