37b539b0b3f2674c6b3dc22a8dea7838612cd5de
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2026-02-08 13:18:45 +0100

    00

---
+++ Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+     public static void main(String[] args) {
+
+        // enkeltlinje-kommentar
+
+
+        /*blokk-kommentar */ 
+
+
+        /** dokumentasjons-kommentar*/ 
+        System.out.println("Program comments are nonexecuting statements you add to a file for documentation");
+     }
+}
