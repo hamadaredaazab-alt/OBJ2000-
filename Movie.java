3fe9b88f9dd99f59c8eaf79b43f7aa1940972920
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2026-02-08 13:07:51 +0100

    3

--- Movie.java
+++ Movie.java
@@ -7,5 +7,5 @@ public static void main(String[] args) {
      System.out.println("Forgiveness is between them and God. It's my job to arrange the meeting.");
         System.out.println("Man on Fire");
         System.out.println("John Creasy (Denzel Washington)");
-        System.out.println("2004");
+        System.out.println("2004");}
 }
